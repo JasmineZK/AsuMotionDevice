dc004e95196da0b5c55134505aeb5de61d0b99ab
zhangkun <zkzkun@yeah.net>
2017-11-12 17:37:08 +0800

    Create project, add .gitignore and .gitattributes

---
+++ AsuDll/Asu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AsuDll
+{
+    /// <summary>
+    /// AsuMotion 封装
+    /// </summary>
+    public class Asu
+    {
+
+    }
+}
